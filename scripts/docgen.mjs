39555183eac482f74719e6337f89b269e081d8d6
pmenage <pauline.menage26@gmail.com>
2024-01-09 11:40:05 +0100

    chore(props): parse all files

--- scripts/docgen.mjs
+++ scripts/docgen.mjs
@@ -1,24 +1,19 @@
 import fs from "fs";
+import path from "node:path";
 import { parse } from "react-docgen-typescript";
 
-// TODO: get all components
-const documentation = parse([
-  "./node_modules/@dev-spendesk/grapes/src/components/Button/Button.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/CollapsibleList/CollapsibleList.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/CollapsibleList/CollapsibleListItem/CollapsibleListItem.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/IconButton/IconButton.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Link/Link.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Modal/Modal.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Modal/ModalOverlay.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Modal/ModalContent.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Modal/ModalHeaderWithIcon.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Modal/ModalHeaderWithIllustration.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Modal/ModalBody.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Modal/ModalFooter.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Tag/Tag.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Toast/Toast.tsx",
-  "./node_modules/@dev-spendesk/grapes/src/components/Tooltip/Tooltip.tsx",
-]);
+const grapesBasePath = "./node_modules/@dev-spendesk/grapes/src/components/";
+
+const tsxFiles = fs
+  .readdirSync(grapesBasePath, {
+    withFileTypes: true,
+    recursive: true,
+  })
+  .filter((file) => file.name.endsWith(".tsx"))
+  .filter((file) => !file.name.includes(".test."))
+  .map((file) => path.join(file.path, file.name));
+
+const documentation = parse(tsxFiles);
 
 fs.writeFile(
   "props.json",
