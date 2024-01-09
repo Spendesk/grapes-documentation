b71a1bd56af73f18259fb15c505ed917ffa941c0
Pauline Ménage <pauline.menage26@gmail.com>
2024-01-09 11:59:42 +0100

    chore(props): improve script (#21)
    * chore(props): remove esno
    
    * chore(props): parse all files

---
+++ scripts/docgen.mjs
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "node:path";
+import { parse } from "react-docgen-typescript";
+
+const grapesBasePath = "./node_modules/@dev-spendesk/grapes/src/components/";
+
+const tsxFiles = fs
+  .readdirSync(grapesBasePath, {
+    withFileTypes: true,
+    recursive: true,
+  })
+  .filter((file) => file.name.endsWith(".tsx"))
+  .filter((file) => !file.name.includes(".test."))
+  .map((file) => path.join(file.path, file.name));
+
+const documentation = parse(tsxFiles);
+
+fs.writeFile(
+  "props.json",
+  `{"props": ${JSON.stringify(documentation)}}`,
+  "utf8",
+  () => {}
+);
