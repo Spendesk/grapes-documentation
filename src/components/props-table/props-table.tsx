3433531c6f0c414a1896ee406107316b6a5d9e66
pmenage <pauline.menage26@gmail.com>
2024-01-09 18:45:11 +0100

    fix(props-table): missing key

--- src/components/props-table/props-table.tsx
+++ src/components/props-table/props-table.tsx
@@ -6,6 +6,7 @@ import { classNames } from "@/utils/classNames";
 import { getSortedProps } from "./utils";
 
 import styles from "./props-table.module.css";
+import { Fragment } from "react";
 
 type Props = {
   components: string[];
@@ -15,10 +16,9 @@ export function PropsTable({ components }: Props) {
   return (
     <div className="mt-s">
       {components.map((component, index) => (
-        <>
+        <Fragment key={index}>
           {components.length > 1 && <h2 id={component}>{component}</h2>}
           <Table
-            key={index}
             className={classNames(
               styles.table,
               components.length > 0 && "mt-m"
@@ -61,7 +61,7 @@ export function PropsTable({ components }: Props) {
             ]}
             data={getSortedProps(component)}
           />
-        </>
+        </Fragment>
       ))}
     </div>
   );
