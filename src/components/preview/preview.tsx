337329a839e7dbd29760e3e450d6116203ad4fee
pmenage <pauline.menage26@gmail.com>
2024-01-10 16:01:01 +0100

    chore(content): add calendar

--- src/components/preview/preview.tsx
+++ src/components/preview/preview.tsx
@@ -1,14 +1,22 @@
 import React, { Suspense } from "react";
 
+import { classNames } from "@/utils/classNames";
+
 import "./preview.css";
 
 type PreviewProps = {
   children: React.ReactNode;
+  isInteractive?: boolean;
 };
 
-export function Preview({ children }: PreviewProps) {
+export function Preview({ children, isInteractive = true }: PreviewProps) {
   return (
-    <div className="docs-preview">
+    <div
+      className={classNames(
+        "docs-preview",
+        !isInteractive && "pointer-events-none"
+      )}
+    >
       <Suspense>{children}</Suspense>
     </div>
   );
