47e1078f1e7580abb050a84d9bbe999349a176f1
DrPlop <thibault.patois@spendesk.com>
2024-01-15 12:46:51 +0100

    feat: implement first draft of the components page (#56)

--- src/app/docs/components/page.tsx
+++ src/app/docs/components/page.tsx
@@ -1,3 +1,38 @@
+import { componentRoutes } from "@/config/routes";
+import Link from "next/link";
+import React from "react";
+import { slug } from "github-slugger";
+
+import "./page.css";
+
 export default function Components() {
-  return <h1>Components</h1>;
+  return (
+    <>
+      <h1>Components</h1>
+      {componentRoutes
+        .filter((route) => route.category !== "Getting Started")
+        .map((route) => {
+          return (
+            <React.Fragment key={route.category}>
+              <h2 id={slug(route.category)}>{route.category}</h2>
+              <div className="docs-component-grid">
+                {route.routes.map((item) => {
+                  return (
+                    <Link href={item.url} key={item.label}>
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
+                      <img
+                        className="w-full h-full object-cover"
+                        src="/bg.avif"
+                        alt="grapes"
+                      />
+                      <p className="m-s">{item.label}</p>
+                    </Link>
+                  );
+                })}
+              </div>
+            </React.Fragment>
+          );
+        })}
+    </>
+  );
 }
