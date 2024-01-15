872836889dad41a936193ba01d093886fc2896f4
DrPlop <thibault.patois@spendesk.com>
2024-01-15 16:08:13 +0100

    refactor: redesign props table

---
+++ src/components/props-table/inline-code.tsx
@@ -0,0 +1,21 @@
+import { codeToHtml, addClassToHast } from "shikiji";
+
+type Props = {
+  code: string;
+};
+
+export async function InlineBlock({ code }: Props) {
+  const html = await codeToHtml(code, {
+    lang: "ts",
+    theme: "slack-ochin",
+    transformers: [
+      {
+        pre(node) {
+          addClassToHast(node, "whitespace-pre-line");
+        },
+      },
+    ],
+  });
+
+  return <span dangerouslySetInnerHTML={{ __html: html }} />;
+}
