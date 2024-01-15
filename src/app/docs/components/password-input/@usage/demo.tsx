d2109d87b37cbba3e50081421b67a60cef12a307
pmenage <pauline.menage26@gmail.com>
2024-01-15 16:49:01 +0100

    docs: add password, switch, textarea, textinput

---
+++ src/app/docs/components/password-input/@usage/demo.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { PasswordInput, type PasswordRule } from "@dev-spendesk/grapes";
+import { useState } from "react";
+
+export const passwordRules: PasswordRule[] = [
+  {
+    label: "characters",
+    required: 12,
+    validate: (value): boolean => !!value && value.length >= 12,
+  },
+  {
+    label: "lowercase",
+    required: 1,
+    validate: (value): boolean => !!value && value.toUpperCase() !== value,
+  },
+  {
+    label: "uppercase",
+    required: 1,
+    validate: (value): boolean => !!value && value.toLowerCase() !== value,
+  },
+  {
+    label: "digit",
+    required: 1,
+    validate: (value): boolean => !!value && /\d/.test(value),
+  },
+];
+
+export const getConfirmPasswordRule = (
+  password: string | null,
+): PasswordRule => {
+  return {
+    label: "Passwords match",
+    validate: (value) => Boolean(password && value && value === password),
+  };
+};
+
+export function DemoPasswordInput() {
+  const [password, setPassword] = useState<string | null>(null);
+  const [confirmPassword, setConfirmPassword] = useState<string | null>(null);
+
+  return (
+    <div className="w-[360px]">
+      <PasswordInput
+        fit="parent"
+        rules={passwordRules}
+        value={password}
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <PasswordInput
+        fit="parent"
+        className="mt-s"
+        rules={[getConfirmPasswordRule(password)]}
+        value={confirmPassword}
+        onChange={(event) => {
+          setConfirmPassword(event.target.value);
+        }}
+      />
+    </div>
+  );
+}
