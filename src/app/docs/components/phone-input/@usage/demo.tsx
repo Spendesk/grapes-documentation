209a702caf81ee596aa17638805f923a26185c75
Pauline Ménage <pauline.menage26@gmail.com>
2024-01-16 14:37:40 +0100

    docs: add phone input (#70)

---
+++ src/app/docs/components/phone-input/@usage/demo.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { PhoneInput } from "@dev-spendesk/grapes";
+import {
+  type CountryCode,
+  getCountryCallingCode,
+  formatIncompletePhoneNumber,
+} from "libphonenumber-js";
+import { useState } from "react";
+
+import { COUNTRIES } from "@/utils/country";
+
+const formatPhoneNumber = (value: string | null, country: string) => {
+  if (!value) {
+    return "";
+  }
+  const formattedPhoneNumber = formatIncompletePhoneNumber(
+    value.replace(/\s/g, ""),
+    country as CountryCode,
+  );
+  const callingCode = getCountryCallingCode(country as CountryCode);
+  if (formattedPhoneNumber === `+${callingCode}`) {
+    return "";
+  }
+  return formattedPhoneNumber.replace(`+${callingCode} `, "");
+};
+
+export function DemoPhoneInput() {
+  const [value, setValue] = useState<string | null>(null);
+  const [country, setCountry] = useState<CountryCode>("FR");
+  return (
+    <div style={{ width: "360px" }}>
+      <PhoneInput
+        formatPhoneNumber={formatPhoneNumber}
+        value={value}
+        callingCode={getCountryCallingCode(country)}
+        country={country}
+        countries={Object.entries(COUNTRIES).map(
+          ([countryCode, countryName]) => ({
+            key: countryCode,
+            label: countryName,
+          }),
+        )}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        onSelectCountry={(selectedCountry) => {
+          setCountry(selectedCountry.key as CountryCode);
+          setValue("");
+        }}
+      />
+    </div>
+  );
+}
