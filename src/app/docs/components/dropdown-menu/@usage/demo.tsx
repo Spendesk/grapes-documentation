d7a96be8908a9eb99a6c97b3bf9f99509a8593a5
Pauline Ménage <pauline.menage26@gmail.com>
2024-01-17 19:26:23 +0100

    docs: add dropdown menu and dropdown item (#78)
    * docs: add dropdown menu and dropdown item
    
    * docs: post review changes

---
+++ src/app/docs/components/dropdown-menu/@usage/demo.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import {
+  Button,
+  CheckboxInput,
+  DropdownItem,
+  DropdownMenu,
+} from "@dev-spendesk/grapes";
+import { useState } from "react";
+
+import { classNames } from "@/utils/classNames";
+
+const costCenters = [
+  { key: "marketing", label: "Marketing" },
+  { key: "legal", label: "Legal" },
+  { key: "office", label: "Office" },
+  { key: "platform", label: "Platform" },
+  { key: "finance", label: "Finance" },
+  { key: "recruitment", label: "Recruitment" },
+];
+
+type Props = {
+  placement?: "bottom-end" | "bottom-start" | "top-end" | "top-start";
+  fitToButton?: boolean;
+};
+
+export function DemoDropdownMenu({ fitToButton, placement }: Props) {
+  const [selectedOption, setSelectedOption] = useState<string | undefined>();
+
+  return (
+    <DropdownMenu
+      options={costCenters}
+      placement={placement}
+      renderButton={(getToggleButtonProps) => (
+        <div className={classNames(fitToButton && "w-[300px]")}>
+          <Button
+            {...getToggleButtonProps()}
+            fit={fitToButton ? "parent" : "content"}
+            variant="ghost"
+            text={placement ?? "Cost centers"}
+          />
+        </div>
+      )}
+      renderOption={(option) => (
+        <DropdownItem
+          label={option.label}
+          isSelected={selectedOption === option.key}
+        />
+      )}
+      onSelect={(option) => {
+        if (option) {
+          setSelectedOption(option.key);
+        }
+      }}
+    />
+  );
+}
+
+export function DemoDropdownMenuCheckboxes() {
+  const [selectedOptions, setSelectedOptions] = useState<string[] | undefined>(
+    undefined,
+  );
+
+  return (
+    <DropdownMenu
+      keepOpenOnSelect
+      renderButton={(getToggleButtonProps) => (
+        <Button
+          {...getToggleButtonProps()}
+          variant="ghost"
+          text="Cost centers"
+        />
+      )}
+      options={costCenters}
+      onSelect={(option) => {
+        if (selectedOptions?.includes(option.key)) {
+          const filteredSelectedOptions = selectedOptions?.filter(
+            (s) => s !== option.key,
+          );
+          setSelectedOptions(
+            filteredSelectedOptions.length
+              ? filteredSelectedOptions
+              : undefined,
+          );
+        } else {
+          setSelectedOptions([...(selectedOptions ?? []), option.key]);
+        }
+      }}
+      renderOption={(option) => {
+        const isSelected = selectedOptions
+          ? selectedOptions.includes(option.key)
+          : false;
+        return (
+          <DropdownItem
+            key={option.key}
+            label={option.label}
+            isSelected={isSelected}
+            isHighlighted={isSelected}
+            prefix={
+              <CheckboxInput isChecked={isSelected} onChange={() => {}} />
+            }
+          />
+        );
+      }}
+    />
+  );
+}
---
+++ src/app/docs/components/popover/@usage/demo.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button, Popover } from "@dev-spendesk/grapes";
+
+type Props = {
+  placement?: "bottom-start" | "bottom-end" | "top-start" | "top-end";
+};
+
+export function DemoPopover({ placement }: Props) {
+  return (
+    <Popover
+      placement={placement}
+      renderTrigger={(triggerProps) => (
+        <Button
+          {...triggerProps}
+          text={placement ?? "Show me a dropdown"}
+          variant="primary"
+        />
+      )}
+    >
+      {(closeDropdown) => (
+        <div className="body-m p-s text-center min-w-[400px]">
+          Hey there!
+          <br />
+          <div>Look at me, I&apos;m a dropdown.</div>
+          <Button
+            className="mt-s"
+            variant="primary"
+            text="Close me"
+            onClick={closeDropdown}
+          />
+        </div>
+      )}
+    </Popover>
+  );
+}
