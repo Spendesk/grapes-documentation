2bb9411314cdfab6e605a63d6206967d3f0c6895
pmenage <pauline.menage26@gmail.com>
2024-01-23 12:35:12 +0100

    fix(typo): add typo box

--- src/app/docs/tokens/typography/typography-tokens.tsx
+++ src/app/docs/tokens/typography/typography-tokens.tsx
@@ -1,12 +1,22 @@
 "use client";
 
-import { Fragment, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   tokens: string[];
 };
 
 export function TypographyTokens({ tokens }: Props) {
+  return (
+    <div className="box flex flex-col gap-m body-m">
+      {tokens.map((token) => (
+        <TypographyBox key={token} token={token} />
+      ))}
+    </div>
+  );
+}
+
+function TypographyBox({ token }: { token: string }) {
   const ref = useRef(null);
   const [fontSize, setFontSize] = useState("");
 
@@ -19,18 +29,14 @@ export function TypographyTokens({ tokens }: Props) {
   }, []);
 
   return (
-    <div className="box flex flex-col gap-m body-m">
-      {tokens.map((token) => (
-        <div className="flex items-center" key={token}>
-          <div className="w-[80px] shrink-0">
-            <div className="font-medium">{token}</div>
-            <div className="text-neutral">{fontSize}</div>
-          </div>
-          <span className={token} ref={ref}>
-            The quick brown fox jumps over the lazy dog
-          </span>
-        </div>
-      ))}
+    <div className="flex items-center">
+      <div className="w-[80px] shrink-0">
+        <div className="font-medium">{token}</div>
+        <div className="text-neutral">{fontSize}</div>
+      </div>
+      <span className={token} ref={ref}>
+        The quick brown fox jumps over the lazy dog
+      </span>
     </div>
   );
 }
