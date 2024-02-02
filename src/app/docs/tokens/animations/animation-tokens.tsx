3323b749ebceddf9eb6af082f1ac2a019f5cc753
Pauline Ménage <pauline.menage26@gmail.com>
2024-02-02 16:13:50 +0100

    fix(animations): fix warning (#134)

--- src/app/docs/tokens/animations/animation-tokens.tsx
+++ src/app/docs/tokens/animations/animation-tokens.tsx
@@ -1,29 +1,56 @@
 "use client";
 
-const animationTokens = [
+import { useEffect, useRef, useState } from "react";
+
+const animationDurations = [
   "--transition-duration",
   "--transition-duration-long",
-  "--transition-timing-function",
 ];
 
+const animationTimingFunction = "--transition-timing-function";
+
 export function AnimationTokens() {
-  if (typeof window === "undefined") {
-    return null;
-  }
-
-  const style = window.getComputedStyle(document.documentElement);
-
   return (
     <div className="flex gap-l box text-center">
-      {animationTokens.map((token) => {
-        const value = style.getPropertyValue(token);
-        return (
-          <div key={token}>
-            <div className="title-m mt-xs">{token}</div>
-            <div className="text-neutral">{value}</div>
-          </div>
-        );
-      })}
+      {animationDurations.map((token) => (
+        <AnimationBox key={token} token={token} property="animation-duration" />
+      ))}
+      <AnimationBox
+        token={animationTimingFunction}
+        property="animation-timing-function"
+      />
+    </div>
+  );
+}
+
+function AnimationBox({
+  token,
+  property,
+}: {
+  token: string;
+  property: string;
+}) {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [animationToken, setAnimationToken] = useState("");
+
+  useEffect(() => {
+    if (ref.current) {
+      setAnimationToken(
+        window.getComputedStyle(ref.current).getPropertyValue(property),
+      );
+    }
+  }, [property]);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        animationDuration: `var(${token})`,
+        animationTimingFunction: `var(${token})`,
+      }}
+    >
+      <div className="title-m mt-xs">{token}</div>
+      <div className="text-neutral">{animationToken}</div>
     </div>
   );
 }
