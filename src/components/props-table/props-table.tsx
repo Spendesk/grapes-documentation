225e5edb6b4b62248e6d395b4bc2b2142508214f
DrPlop <thibault.patois@spendesk.com>
2024-02-22 16:28:29 +0100

    feat(props): add link to icon page when prop refers to icons

--- src/components/props-table/props-table.tsx
+++ src/components/props-table/props-table.tsx
@@ -1,11 +1,12 @@
 import { ListView, ListItem, Tag } from "@dev-spendesk/grapes";
+import Link from "next/link";
+import { Fragment } from "react";
 import { classNames } from "@/utils/classNames";
 
 import { getSortedProps } from "./utils";
 import { InlineBlock } from "./inline-code";
 
 import styles from "./props-table.module.css";
-import { Fragment } from "react";
 
 type Props = {
   components: string[];
@@ -20,17 +21,32 @@ export function PropsTable({ components }: Props) {
           <ListView className={classNames(components.length > 0 && "mt-m")}>
             {getSortedProps(component).map(
               ({ name, required, type, defaultValue, description }) => {
+                const isAnIconProp =
+                  type.raw && type.raw.includes("arrow-bottom");
+
                 return (
                   <ListItem key={name} className={styles.listItem}>
                     <div className="flex items-baseline gap-xs ">
-                      <span
-                        className={classNames(
-                          "text-complementary title-l",
-                          required && styles.required,
-                        )}
-                      >
-                        {name}
-                      </span>
+                      {isAnIconProp ? (
+                        <Link
+                          className={classNames(
+                            "text-complementary title-l",
+                            required && styles.required,
+                          )}
+                          href="/docs/components/icon"
+                        >
+                          {name}
+                        </Link>
+                      ) : (
+                        <span
+                          className={classNames(
+                            "text-complementary title-l",
+                            required && styles.required,
+                          )}
+                        >
+                          {name}
+                        </span>
+                      )}
                       <InlineBlock code={type.raw ?? type.name} />
                     </div>
                     <p className="text-neutral-dark mt-xs">{description}</p>
