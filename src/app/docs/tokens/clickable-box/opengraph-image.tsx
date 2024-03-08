fa162c9e28e655e86e60c4f7a496ff7b67f9f476
Pauline Ménage <pauline.menage26@gmail.com>
2024-03-08 11:03:29 +0100

    chore(clickable-box): add documentation (#227)

---
+++ src/app/docs/tokens/clickable-box/opengraph-image.tsx
@@ -0,0 +1,22 @@
+import { ImageResponse } from "next/og";
+
+import OgImage, { fetchFonts } from "@/components/og-image/ogImage";
+
+export const alt = "Design Tokens: Clickable Box";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export default async function Image() {
+  const fonts = await fetchFonts();
+  return new ImageResponse(
+    <OgImage title="Clickable Box" subtitle="Design Tokens" />,
+    {
+      ...size,
+      fonts,
+    },
+  );
+}
