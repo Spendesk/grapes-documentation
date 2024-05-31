8afc851425121a2dfcb07cab4f3e3cc7a37d31cb
pmenage <pauline.menage26@gmail.com>
2024-05-31 16:46:28 +0200

    feat(typo): add title-xxl

--- src/app/docs/tokens/typography/types.ts
+++ src/app/docs/tokens/typography/types.ts
@@ -1,3 +1,9 @@
 export const bodyTokens = ["body-s", "body-m", "body-l", "body-xl", "body-xxl"];
 
-export const titleTokens = ["title-s", "title-m", "title-l", "title-xl"];
+export const titleTokens = [
+  "title-s",
+  "title-m",
+  "title-l",
+  "title-xl",
+  "title-xxl",
+];
