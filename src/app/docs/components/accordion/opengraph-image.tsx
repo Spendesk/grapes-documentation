d57b95e8d9dc78daf31f7ceaa8cc5c224726e6e6
DrPlop <thibault.patois@spendesk.com>
2024-10-25 13:28:12 +0200

    fix: add accessibility to timeline and image to accordion

---
+++ src/app/docs/components/accordion/opengraph-image.tsx
@@ -0,0 +1,19 @@
+import { ImageResponse } from "next/og";
+
+import OgImage, { fetchFonts } from "@/lib/og-image/ogImage";
+
+export const alt = "Component: Accordion";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export default async function Image() {
+  const fonts = await fetchFonts();
+  return new ImageResponse(<OgImage title="Accordion" subtitle="Component" />, {
+    ...size,
+    fonts,
+  });
+}
