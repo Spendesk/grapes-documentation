b9b72030bd7eca0fba1d47d9eb51a64edaabec2d
Pauline Ménage <pauline.menage26@gmail.com>
2024-12-04 12:08:01 +0100

    docs(ui-refresh): update tag (#600)
    * docs(ui-refresh): update tag
    
    * docs: post review changes

---
+++ src/app/docs/components/tag/@usage/demo.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Tag } from "@dev-spendesk/grapes";
+
+export function Demo() {
+  return (
+    <Tag variant="purple" onClose={() => {}}>
+      My tag
+    </Tag>
+  );
+}
