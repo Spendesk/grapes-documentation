ae0f7af4550c493ba71c4cede5157ac96fe4dc62
Pauline Ménage <pauline.menage26@gmail.com>
2024-12-04 15:40:05 +0100

    docs(ui-refresh): update border radius (#609)

--- src/app/docs/tokens/border-radius/types.ts
+++ src/app/docs/tokens/border-radius/types.ts
@@ -1,1 +1,5 @@
-export const borderRadiusTokens = ["--border-radius-xxs", "--border-radius-xs"];
+export const borderRadiusTokens = [
+  "--border-radius-4",
+  "--border-radius-8",
+  "--border-radius-12",
+];
