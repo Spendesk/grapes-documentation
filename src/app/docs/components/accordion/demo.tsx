f3c82b4ab7034efbd0cc3f299c7eb4117377ff91
DrPlop <thibault.patois@spendesk.com>
2025-01-30 18:04:51 +0100

    fix: migrate also the accordion documentation

--- src/app/docs/components/accordion/demo.tsx
+++ src/app/docs/components/accordion/demo.tsx
@@ -1,73 +1,71 @@
 "use client";
 
-import {
-  Accordion,
-  AccordionHandle,
-  AccordionItem,
-  Button,
-} from "@dev-spendesk/grapes";
-import { useRef } from "react";
+import { Accordion, AccordionItem, Button } from "@dev-spendesk/grapes";
+import { useState } from "react";
 
-const accordionItems = [
-  "Invite team members",
-  "Make a first payment",
-  "Order physical cards",
-];
+export function DemoAccordion() {
+  return (
+    <Accordion className="w-full overflow-hidden">
+      <AccordionItem renderHeader={() => "Invite team members"}>
+        <div className="p-24 pt-8 text-primary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+          lectus sed sem porttitor viverra. Vestibulum magna leo.
+        </div>
+      </AccordionItem>
+      <AccordionItem renderHeader={() => "Make a first payment"}>
+        <div className="p-24 pt-8 text-primary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+          lectus sed sem porttitor viverra. Vestibulum magna leo.
+        </div>
+      </AccordionItem>
+    </Accordion>
+  );
+}
 
-type Props = {
-  withMultipleOpenItems?: boolean;
-  withDefaultOpenItems?: boolean;
-  withOpenClose?: boolean;
-};
+export function DemoOpen() {
+  return (
+    <Accordion className="w-full text-primary">
+      <AccordionItem open renderHeader={() => "Invite team members"}>
+        <div className="p-24 pt-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+          lectus sed sem porttitor viverra. Vestibulum magna leo.
+        </div>
+      </AccordionItem>
+      <AccordionItem renderHeader={() => "Make a first payment"}>
+        <div className="p-24 pt-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+          lectus sed sem porttitor viverra. Vestibulum magna leo.
+        </div>
+      </AccordionItem>
+    </Accordion>
+  );
+}
 
-export function DemoAccordion({
-  withMultipleOpenItems,
-  withDefaultOpenItems,
-  withOpenClose,
-}: Props) {
-  const accordionRef = useRef<AccordionHandle | null>(null);
-  const openAccordion = () => {
-    accordionRef.current?.openAccordion(new Set([0, 1, 2]));
-  };
-  const closeAccordion = () => {
-    accordionRef.current?.closeAccordion();
-  };
+export function DemoControlledAccordion() {
+  const [indexOpen, setIndexOpen] = useState("close"); // [!code highlight]
 
   return (
     <>
-      {withOpenClose && (
-        <>
-          <Button
-            variant="primaryBrand"
-            text="Open accordion"
-            onClick={() => openAccordion()}
-          />
-          <Button
-            variant="primaryBrand"
-            text="Close accordion"
-            onClick={() => closeAccordion()}
-          />
-        </>
-      )}
-      <Accordion
-        ref={accordionRef}
-        allowMultipleOpenItems={withMultipleOpenItems}
-        defaultAccordionItemIndexes={
-          withDefaultOpenItems ? new Set([0, 2]) : undefined
-        }
-        className="w-full overflow-hidden"
-      >
-        {accordionItems.map((accordionItem) => (
-          <AccordionItem
-            key={accordionItem}
-            renderHeader={() => <div>{accordionItem}</div>}
-          >
-            <div className="p-24 pt-8 text-primary">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-              lectus sed sem porttitor viverra. Vestibulum magna leo.
-            </div>
-          </AccordionItem>
-        ))}
+      <Button
+        variant="primaryBrand"
+        text="Open accordion"
+        onClick={() => setIndexOpen("open")}
+      />
+      <Button
+        variant="primaryBrand"
+        text="Close accordion"
+        onClick={() => setIndexOpen("close")}
+      />
+      <Accordion className="w-full overflow-hidden">
+        <AccordionItem
+          open={"open" === indexOpen} // [!code highlight]
+          renderHeader={() => "Invite team members"}
+        >
+          <div className="p-24 pt-8 text-primary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+            lectus sed sem porttitor viverra. Vestibulum magna leo.
+          </div>
+        </AccordionItem>
       </Accordion>
     </>
   );
