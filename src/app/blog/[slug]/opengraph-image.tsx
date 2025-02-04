d6d74971d3fffb4b57ccddf96e1d3207db48813b
DrPlop <thibault.patois@spendesk.com>
2025-02-04 18:23:07 +0100

    docs(blog): generate opengraph image for the blog (#731)

---
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -0,0 +1,43 @@
+import { ImageResponse } from "next/og";
+
+import OgImage, { fetchFonts } from "@/lib/og-image/ogImage";
+import { getBlogPosts } from "../utils";
+
+export const alt = "Blog";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+export const revalidate = false;
+
+export async function generateStaticParams() {
+  let posts = getBlogPosts();
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+export default async function Image({ params }: Props) {
+  const slug = (await params).slug;
+
+  let post = getBlogPosts().find((post) => post.slug === slug);
+
+  if (!post) {
+    throw new Error("Unable to find post");
+  }
+
+  const fonts = await fetchFonts();
+  return new ImageResponse(
+    <OgImage title={post.metadata.title} subtitle="Blog" />,
+    {
+      ...size,
+      fonts,
+    },
+  );
+}
