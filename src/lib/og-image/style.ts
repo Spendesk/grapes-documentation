d6907a4af3de2a4aad524758203735c13ac45e69
DrPlop <thibault.patois@spendesk.com>
2025-02-11 18:16:48 +0100

    chore(image): update - again - opengraph image design

--- src/lib/og-image/style.ts
+++ src/lib/og-image/style.ts
@@ -1,21 +1,90 @@
 import type { CSSProperties } from "react";
 
-export const ogTitle: CSSProperties = {
-  fontSize: "60px",
-  color: "#191b1b",
-  fontFamily: "Inter",
-  lineHeight: "70px",
-  padding: 0,
-  margin: 0,
-};
-
 export const ogRoot: CSSProperties = {
   display: "flex",
   flexDirection: "column",
-  justifyContent: "center",
-  gap: "32px",
-  padding: "64px",
+  justifyContent: "space-between",
+  alignItems: "stretch",
+  backgroundColor: "white",
   width: "100%",
   height: "100%",
-  background: "radial-gradient(circle at 120% 120%,  #7136ed, white 55%)",
+};
+
+export const ogContainer: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+  flexGrow: 1,
+};
+
+export const ogColorContainer: CSSProperties = {
+  display: "flex",
+  padding: "64px",
+  flexGrow: 1,
+  height: "100%",
+  borderRight: "1px solid #191b1b",
+};
+
+export const ogColorBlock1: CSSProperties = {
+  flexGrow: 2,
+  backgroundColor: "#7136ed",
+  height: "100%",
+  borderRadius: "12px 0 0 12px",
+};
+export const ogColorBlock2: CSSProperties = {
+  flexGrow: 2,
+  backgroundColor: "#C5B6FF",
+  height: "100%",
+};
+export const ogColorBlock3: CSSProperties = {
+  flexGrow: 1,
+  backgroundColor: "#edd8f8",
+  height: "100%",
+};
+export const ogColorBlock4: CSSProperties = {
+  flexGrow: 1,
+  backgroundColor: "#DFAFF7",
+  height: "100%",
+};
+export const ogColorBlock5: CSSProperties = {
+  flexGrow: 1,
+  backgroundColor: "#C1CAFB",
+  height: "100%",
+};
+export const ogColorBlock6: CSSProperties = {
+  flexGrow: 1,
+  backgroundColor: "#9696EE",
+  height: "100%",
+};
+export const ogColorBlock7: CSSProperties = {
+  flexGrow: 1,
+  backgroundColor: "#E4E6A2",
+  height: "100%",
+};
+export const ogColorBlock8: CSSProperties = {
+  flexGrow: 1,
+  backgroundColor: "#CED25D",
+  height: "100%",
+  borderRadius: "0 12px 12px 0",
+};
+
+export const ogLogoContainer: CSSProperties = {
+  display: "flex",
+  padding: "64px",
+};
+
+export const ogTitleContainer: CSSProperties = {
+  display: "flex",
+  padding: "64px",
+  borderTop: "1px solid #191b1b",
+};
+
+export const ogTitle: CSSProperties = {
+  fontSize: "90px",
+  color: "#191b1b",
+  fontFamily: "Roobert",
+  lineHeight: "120px",
+  padding: 0,
+  margin: 0,
 };
